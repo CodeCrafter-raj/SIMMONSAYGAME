48ced49753e3527d808ffd071fd0c161bda4e090
blowkme <92511792+blowkme@users.noreply.github.com>
2023-09-13 02:18:23 +0530

    Add files via upload

---
+++ sim.js
@@ -0,0 +1,33 @@
+let gameseq=[];
+let userseq=[];
+
+let started=false;
+let level=0;
+
+let h2=document.querySelector("h2");
+
+document.addEventListener("keypress" ,function()
+{
+    if(started==false)
+    {
+        console.log("game is started");
+        started=true;
+
+        levelUp()
+    }
+});
+
+
+
+
+function btnFlash(btn){
+    btn.classList.add("flash")
+
+}
+function levelUp()
+{
+    level++;
+    h2.innerText=`Level ${level}`;
+
+    btnFlash();
+}
